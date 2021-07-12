e99f58dbeb334696a7ea1d95de8d07e5ae412eb0
KunalSharma8 <75616285+KunalSharma8@users.noreply.github.com>
2021-07-12 11:24:06 +0530

    multiple

--- CV2Code1.py
+++ CV2Code1.py
@@ -2,3 +2,12 @@ def func():
 	print("r")
 
 func()	
+
+
+class Vector:
+        def __init__(self, values):
+                self._values = values
+
+        def __eq__(self, other):
+                return tuple(self) == tuple(other)
+        
